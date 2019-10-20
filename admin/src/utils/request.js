860abc13cb285144a0146485c5cf74f0cb99282f
burgess <wurui_0@163.com>
2019-10-21 02:34:55 +0800

    update

--- admin/src/utils/request.js
+++ admin/src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 import Qs from 'qs'
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://127.0.0.1:8080', // url = base url + request url
+  baseURL: 'http://159.138.135.12:8080', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 6000, // request timeout
   transformRequest: [function (data) {
